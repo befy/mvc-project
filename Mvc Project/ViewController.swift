7687ce05634dfe251f24e93775a51aa4e06a9a6d
Alper Tabak <mehmetalpertabak@gmail.com>
2018-09-01 16:02:10 +0300

    start

---
+++ Mvc Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mvc Project
+//
+//  Created by Alper Tabak on 1.09.2018.
+//  Copyright © 2018 Alper Tabak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
